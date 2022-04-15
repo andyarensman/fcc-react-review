ddc7fa0d7f252b2f8486f0d6213597571baf65b0
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-04-15 13:31:16 -0700

    Update redux.js

--- redux.js
+++ redux.js
@@ -298,3 +298,137 @@ const store = Redux.createStore(
   asyncDataReducer,
   Redux.applyMiddleware(ReduxThunk.default)
 );
+
+//COUNTERWITH REDUX
+
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for decrement action types
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1
+    default:
+      return state
+  }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+  return {type: INCREMENT};
+}; // Define an action creator for incrementing
+
+const decAction = () => {
+  return {type: DECREMENT};
+};; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
+
+//NEVER MUTATE STATE
+
+const ADD_TO_DO = 'ADD_TO_DO';
+
+// A list of strings representing tasks to do:
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch(action.type) {
+    case ADD_TO_DO:
+      // Don't mutate state here or the tests will fail
+      var newtoDos = todos.concat(action.todo)
+      return newtoDos
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+//SPREAD OPERATOR
+
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // Don't mutate state here or the tests will fail
+      var newtodo = [...state]
+      newtodo.push(action.todo)
+      console.log(newtodo)
+      return newtodo
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+//REMOVE ITEM FROM ARRAY
+
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      // Don't mutate state here or the tests will fail
+      var newstate = [...state]
+      newstate.splice(action.index, 1)
+      console.log(newstate)
+      return newstate
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+//COPY OBECT WITH Object.assign
+
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      var newstate = Object.assign({}, state, {status: 'online'})
+      console.log(newstate)
+      return newstate
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
